cc76ba6b99c30d1829099d3bf933312985a399a2
HoucemZaier <Houcem.engineering@gmail.com>
2024-11-29 14:19:30 +0100

    Update loginHandler.php

--- user/frontOffice/loginHandler.php
+++ user/frontOffice/loginHandler.php
@@ -1,27 +1,25 @@
 
 <?php
 
-// Inclure le contrôleur
 include $_SERVER['DOCUMENT_ROOT'] . '/projet-web/BackOffice/controller/UserController.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $prenom = $_POST['firstName'];
-    $nom = $_POST['lastName'];
 
-    // Instancier le contrôleur utilisateur
+    $prenom = trim($_POST['firstName']);
+    $nom = trim($_POST['lastName']);  
+
     $userController = new UserController();
 
-    // Récupérer l'utilisateur
     $user = $userController->getUserByName($nom, $prenom);
 
-    if ($user) {
-        // Si l'utilisateur existe, afficher un message de bienvenue
+    if ($user && isset($user['nom']) && isset($user['prenom'])) {
         echo "Welcome, " . htmlspecialchars($user['nom']) . " " . htmlspecialchars($user['prenom']) . "!";
     } 
+
     else {
-        // Redirection sécurisée vers le formulaire utilisateur
         header("Location: /projet-web/BackOffice/view/userForm.php");
         exit();
     }
 }
+
 ?>
